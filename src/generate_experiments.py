7839ff3550ca0dd7be5900d71ade424a94130734
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-04-22 10:55:34 +0100

    generate experiments updated for exp delpoyment

--- src/generate_experiments.py
+++ src/generate_experiments.py
@@ -11,36 +11,38 @@ SCRATCH_HOME = f'{SCRATCH_DISK}/{USER}'
 
 DATA_HOME = f'{SCRATCH_HOME}/robotic_constraints'
 base_call = (f"python main_experiment_avoid.py --input {DATA_HOME}/data --output {DATA_HOME}/output "
-             "--use_cuda True --batch_size 256 --num_epochs 500 --early-stopping-lim 50 --num_workers 8")
+             "--use_cuda True --batch_size 256 --num_epochs 2500 --early-stopping-lim 50 --num_workers 8 "
+             "--max_grad_norm 1")
 
 repeats = 1
-learning_rates = [1e-3, 1e-4]
-num_layers = [50, 70, 100]
-gammas = [.9, .95, .99]
+learning_rates = [1e-2, 1e-3, 1e-4]
+hidden_dim = [40, 50, 60, 70, 80]
+gammas = [.9, .95, .99, .999]
 backward = [True, False]
-# learning_rates = [1e-4]
+back_strength = [1e2, 1e3, 1e4, 1e5]
 # num_layers = [40]
 # gammas = [.99]
 
-settings = [(lr, gam, rep, lay, back_) for lr in learning_rates for gam in gammas for lay in num_layers for back_ in backward
+settings = [(lr, gam, h_dim, bk, back_, rep) for lr in learning_rates for gam in gammas for h_dim in hidden_dim for bk in back_strength for back_ in backward
             for rep in range(repeats)]
-nr_expts = len(learning_rates) * len(gammas) * len(num_layers) * repeats
+nr_expts = len(learning_rates) * len(hidden_dim) * len(gammas) * len(backward) * len(back_strength) * repeats
 
 nr_servers = 15
-avg_expt_time = 45  # mins
+avg_expt_time = 5  # mins
 print(f'Total experiments = {nr_expts}')
 print(f'Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs')
 
 output_file = open("experiment.txt", "w")
 
-for lr, gam, rep, lay, back_ in settings:
+for (lr, gam, h_dim, bk, back_, rep) in settings:
     # Note that we don't set a seed for rep - a seed is selected at random
     # and recorded in the output data by the python script
     expt_call = (
         f"{base_call} "
         f"--lr {lr} "
         f"--gamma {gam} "
-        f"--num_layers {lay} "
+        f"--hidden_dim {h_dim} "
+        f"--back_strength {bk} "
         f"--backward {back_}"
     )
     print(expt_call, file=output_file)
