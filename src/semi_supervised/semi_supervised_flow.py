2f1ee121b23c9745fec13058ef69c09a5fad00f2
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-04-28 06:53:06 +0100

    massive restructuring of code for less repetition of train and test boilerplate

--- src/semi_supervised/semi_supervised_flow.py
+++ src/semi_supervised/semi_supervised_flow.py
@@ -1,21 +1,24 @@
+"""
+Author: Nick Hoernle
+Base class that defines the main, train, test methods that are used by the ML models in this repository. 
+Each experimet will extend this class to define it's own parameters for the experiment but this class allows 
+for more efficiency in the code.
+"""
+
 from __future__ import print_function
-import argparse
-import torch
-import itertools
-import numpy as np
 
 import os
 import random
 import logging
 
-import torch.utils.data
-from torch import nn, optim
-from torch.nn import functional as F
+import numpy as np
+import torch
+from torch import optim
 from torchvision import datasets, transforms
-from tqdm import tqdm
-from torchvision.utils import save_image
-from torch.distributions import MultivariateNormal, Uniform, \
-    TransformedDistribution, SigmoidTransform, Normal, Categorical
+
+
+from utils.logging import raise_cuda_error, AverageMeter
+from utils.generative_trainer import GenerativeTrainer
 
 from nflib import (AffineConstantFlow,
     AffineConstantFlow, ActNorm, AffineHalfFlow,
@@ -24,14 +27,7 @@ from nflib import (AffineConstantFlow,
     BaseDistribution, BaseDistributionMixtureGaussians,
     SS_Flow
 )
-from utils.data import get_samplers, to_logits, convert_to_one_hot
 
-from utils.logging_utils import AverageMeter, raise_cuda_error
-
-best_loss = np.inf
-global_step = 0
-NUM_CATEGORIES = 5
-mdl_name = 'vae'
 
 def build_model(dim=10, num_layers=10, conditioning=True, num_conditioning=10):
 
@@ -42,137 +38,58 @@ def build_model(dim=10, num_layers=10, conditioning=True, num_conditioning=10):
     convs = [Invertible1x1Conv(dim=dim) for _ in flows]
     norms = [ActNorm(dim=dim) for _ in flows]
 
-    flows = list(itertools.chain(*zip(flows, convs, norms))) # append a coupling layer after each 1x1
+    flows = list(itertools.chain(*zip(flows, convs, norms)))
 
     return NormalizingFlowModel(base_dist, flows, conditioning=conditioning)
 
+class CategoricalFlowGenerativeTrainer(GenerativeTrainer):
+    def __init__(self, model_builder, model_parameters, input_data, output_data, dataset='MNIST', max_grad_norm=1, num_epochs=100, batch_size=256, lr=1e-3, use_cuda=True, num_test_samples=256, seed=0, gamma=0.9, resume=False, early_stopping_lim=50, additional_model_config_args=[], num_loader_workers=8, data_shuffle=True, name="base-model",):
 
-def main(args):
-    global best_loss
+        train_ds, valid_ds, NUM_CATEGORIES = get_dataset(input_data, dataset)
+        super().__init__(model_builder, model_parameters, (train_ds, valid_ds), output_data, max_grad_norm=1, num_epochs=100, batch_size=256, lr=1e-3, use_cuda=True, num_test_samples=256, seed=0, gamma=0.9, resume=False, early_stopping_lim=50, additional_model_config_args=[], num_loader_workers=8, data_shuffle=True, name="base-model",)
 
-    # Set up main device and scale batch size
-    use_cuda = args.use_cuda and torch.cuda.is_available()
-    if args.use_cuda and not use_cuda:
-        raise_cuda_error()
+        self.dataset = dataset # Must be one of MNIST|CIFAR10|CIFAR100'
+        self.NUM_CATEGORIES = NUM_CATEGORIES
 
-    device = torch.device("cuda" if use_cuda else "cpu")
-    if use_cuda:
-        logging.info(f'Using device: {torch.cuda.get_device_name()}')
+    @torch.enable_grad()
+    def train(self, epoch, optimizer, **kwargs):
+        """
+        Train step of model returned by model_builder
+        """
+        net = self.net
 
-    config_args = [str(vv) for kk, vv in vars(args).items()
-                   if kk in ['batch_size', 'lr', 'gamma', "hidden_dim", 'seed', "backward", "back_strength"]]
-    model_name = '_'.join(config_args)
+        net.train()
+        loss_meter = AverageMeter()
 
-    if not os.path.exists(args.output):
-        logging.info(f'{args.output} does not exist, creating...')
-        os.makedirs(args.output)
+        (train_loader_labelled_, train_loader) = trainloader
+        train_loader_labelled = iter(train_loader_labelled_)
 
-    # Set random seeds
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    torch.cuda.manual_seed_all(args.seed)
+    @torch.no_grad()
+    def test(self, epoch, optimizer, **kwargs):
+        """
+        Test step of model returned by model_builder
+        """
+        raise NotImplementedError(
+            "Class must be overridden and test should be implemented"
+        )
 
-    # Generators
-    loader_params = {'batch_size': args.batch_size, 'shuffle': False, 'num_workers': args.num_workers}
-    if args.dataset == 'MNIST':
-        train_ds = datasets.MNIST(args.input, train=True, download=True, transform=transforms.ToTensor())
-        test_ds = datasets.MNIST(args.input, train=False, transform=transforms.ToTensor())
-        NUM_CATEGORIES=10
-    elif args.dataset == 'CIFAR10':
-        train_ds = datasets.MNIST(args.input, train=True, download=True, transform=transforms.ToTensor())
-        test_ds = datasets.MNIST(args.input, train=False, transform=transforms.ToTensor())
-        NUM_CATEGORIES=10
-    elif args.dataset == 'CIFAR100':
-        train_ds = datasets.MNIST(args.input, train=True, download=True, transform=transforms.ToTensor())
-        test_ds = datasets.MNIST(args.input, train=False, transform=transforms.ToTensor())
-        NUM_CATEGORIES=100
+
+def get_dataset(input_data, dataset):
+    if dataset == 'MNIST':
+        train_ds = datasets.MNIST(input_data, train=True, download=True, transform=transforms.ToTensor())
+        valid_ds = datasets.MNIST(input_data, train=False, transform=transforms.ToTensor())
+        num_categories = 10
+    elif dataset == 'CIFAR10':
+        train_ds = datasets.MNIST(input_data, train=True, download=True, transform=transforms.ToTensor())
+        valid_ds = datasets.MNIST(input_data, train=False, transform=transforms.ToTensor())
+        num_categories = 10
+    elif dataset == 'CIFAR100':
+        train_ds = datasets.MNIST(input_data, train=True, download=True, transform=transforms.ToTensor())
+        valid_ds = datasets.MNIST(input_data, train=False, transform=transforms.ToTensor())
+        num_categories = 100
     else:
         raise ValueError("Dataset not in {MNIST|CIFAR10|CIFAR100}")
-
-    labelled_sampler, unlabelled_sampler = get_samplers(train_ds.train_labels.numpy(), n=100, n_categories=NUM_CATEGORIES)
-    train_loader_labelled = torch.utils.data.DataLoader(train_ds, sampler=labelled_sampler, **loader_params)
-    train_loader = torch.utils.data.DataLoader(train_ds, sampler=unlabelled_sampler, **loader_params)
-    test_loader = torch.utils.data.DataLoader(test_ds, **loader_params)
-
-    # length = 0
-    # for data_u, labels_u in train_loader:
-    #     length += data_u.size(0)
-    #
-    # print(length)
-
-    # Model
-    dims = 1
-    for i in train_loader.dataset.__getitem__(0)[0].shape:
-        dims *= i
-
-    print(f'Building model with {dims} latent dims')
-    # net = VAE_Categorical(data_dim=dims, hidden_dim=args.hidden_dim, NUM_CATEGORIES=NUM_CATEGORIES)
-    net = build_model(args, data_dim=dims)
-
-    print("number of params: ", sum(p.numel() for p in net.parameters()))
-    net = net.to(device)
-
-    start_epoch = 0
-    save_dir = os.path.join(args.output, 'models')
-
-    if args.resume:
-        # Load checkpoint.
-        print('Resuming from checkpoint at save/best.pth.tar...')
-        assert os.path.isdir(save_dir), 'Error: no checkpoint directory found!'
-        checkpoint = torch.load(os.path.join(save_dir, f'{mdl_name}_{model_name}.best.pt'))
-        net.load_state_dict(checkpoint['net'])
-        global global_step
-        best_loss = checkpoint['test_loss']
-        global_step = start_epoch * len(train_loader.dataset)
-
-    optimizer = optim.Adam(net.parameters(), lr=args.lr)
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=args.gamma)
-
-    os.makedirs(f'{args.output}/logs/', exist_ok=True)
-    log_fh = open(f'{args.output}/logs/{mdl_name}_{model_name}.log', 'w')
-    count_valid_not_improving = 0
-
-    for epoch in range(start_epoch, start_epoch + args.num_epochs):
-        loss = train(epoch, net, (train_loader_labelled, train_loader), device, optimizer, scheduler, args.max_grad_norm, args, dims)
-        vld_loss = test(epoch, net, test_loader, device, args.num_samples, save_dir, args, model_name, dims)
-
-        scheduler.step()
-
-        net.tau = np.max((0.5, net.tau * np.exp(-5e-3 * (epoch))))
-
-        if not np.isnan(vld_loss):
-            log_fh.write(f'{epoch},{loss},{vld_loss}\n')
-            log_fh.flush()
-
-        # early stopping
-        if vld_loss >= best_loss:
-            count_valid_not_improving += 1
-
-        if count_valid_not_improving > args.early_stopping_lim:
-            print(f'Early stopping implemented at epoch #: {epoch}')
-            break
-        else:
-            count_valid_not_improving = 0
-
-        if np.isnan(vld_loss):
-            print(f'Early stopping: valid loss is NAN')
-            break
-
-        with torch.no_grad():
-
-            labels = torch.zeros(64, NUM_CATEGORIES).to(device)
-            labels[torch.arange(64), torch.arange(8).repeat(8)] = 1
-
-            img_sample = net.sample_labelled(labels)
-
-            img_sample = torch.sigmoid(img_sample)
-            save_image(img_sample.view(64, 1, 28, 28), '../experiments/vaelib/samples/sample_' + str(epoch) + '.png')
-
-    log_fh.close()
-
-    state_curr = {'net': net.state_dict()}
-    torch.save(state_curr, os.path.join(save_dir, f'{mdl_name}_{model_name}.final.pt'))
+    return train_ds, valid_ds, num_categories
 
 
 def labelled_loss(data, data_reconstructed, latent_params, label_sample):
@@ -183,6 +100,7 @@ def labelled_loss(data, data_reconstructed, latent_params, label_sample):
 def unlabelled_loss(data, data_reconstructed, latent_params, label_sample):
     #TODO
     return 0
+
 
 @torch.enable_grad()
 def train(epoch, net, trainloader, device, optimizer, scheduler, max_grad_norm, args, dims):
---
+++ src/utils/generative_trainer.py
@@ -0,0 +1,248 @@
+"""
+Author: Nick Hoernle
+Base class that defines the main, train, test methods that are used by the ML models in this repository. 
+Each experimet will extend this class to define it's own parameters for the experiment but this class allows 
+for more efficiency in the code.
+"""
+
+from __future__ import print_function
+
+import os
+import random
+import logging
+
+import numpy as np
+import torch
+from torch import optim
+
+from utils.logging import raise_cuda_error
+
+
+class GenerativeTrainer:
+    """
+    Base class that defines the main, train, test methods that are used by the ML models in this repository. 
+    Each experimet will extend this class to define it's own parameters for the experiment but this class allows 
+    for more efficiency in the code.
+    """
+
+    def __init__(
+        self,
+        model_builder,
+        model_parameters,
+        input_torch_datasets,
+        output_data,
+        max_grad_norm=1,
+        num_epochs=100,
+        batch_size=256,
+        lr=1e-3,
+        use_cuda=True,
+        num_test_samples=256,
+        seed=0,
+        gamma=0.9,
+        resume=False,
+        early_stopping_lim=50,
+        additional_model_config_args=[],
+        num_loader_workers=8,
+        data_shuffle=True,
+        name="base-model",
+    ):
+
+        # Returns a class that extends nn.Model that will be used for training. Implements .forward and .backward
+        self.model_builder = model_builder
+        # Parameters that are direclty related to the model init. Passed to model_builder
+        self.model_parameters = model_parameters
+        self.global_step = 0  # Starting step of the iteration
+        self.best_loss = (
+            np.inf
+        )  # Starting best validation loss that is used for early stopping and logging
+
+        self.output_data_path = output_data  # Path to where logs, models are saved
+
+        # training parameters
+        self.max_grad_norm = max_grad_norm
+        self.num_epochs = num_epochs  # Number of epochs to train
+        self.batch_size = batch_size  # Batch size
+        self.lr = lr  # Peak learning rate
+        self.seed = seed  # Random seed for reproducibility
+        self.gamma = gamma  # Learning rate step gamma (default: 0.9)
+        self.resume = resume  # Resume from checkpoint
+        # Early stopping implemented after N epochs with no improvement (default: 50)
+        self.early_stopping_lim = early_stopping_lim
+        self.additional_model_config_args = (
+            additional_model_config_args  # list of config args to include in logging
+        )
+        self.name = name  # Basic evaluator interpretable name of model
+
+        self.num_loader_workers = (
+            num_loader_workers  # Number of workers to use for the dataset loaders
+        )
+        # Whether or not to shuffle the data for the loaders. Not compatible with samplers if semi-supervised training
+        self.data_shuffle = data_shuffle
+
+        self.num_test_samples = num_test_samples  # Number of samples at test time
+
+        self.device_loaded = False
+        self.device = init_device(use_cuda)
+
+        # init loaders
+        loader_params = {
+            "batch_size": self.batch_size,
+            "shuffle": self.data_shuffle,
+            "num_workers": num_loader_workers,
+        }
+        train_ds, valid_ds = input_torch_datasets  # tuple of train, test torch datasets
+
+        self.train_loader = torch.utils.data.DataLoader(train_ds, **loader_params)
+        self.valid_loader = torch.utils.data.DataLoader(valid_ds, **loader_params)
+
+        self.data_dims = 1
+        for i in self.train_loader.dataset.__getitem__(0)[0].shape:
+            self.data_dims *= i
+
+        self.network_loaded = False
+        self.net = self.model_builder(**self.model_parameters)
+        logging.info(
+            "number of params: ", sum(p.numel() for p in self.net.parameters())
+        )
+
+        self.net = self.net.to(self.device)
+
+        # logging
+        create_folder_if_not_exists(self.output_data_path)
+        self.logger_path = create_folder_if_not_exists(f"{self.output_data_path}/logs/")
+        self.models_path = create_folder_if_not_exists(
+            f"{self.output_data_path}/models/"
+        )
+        self.log_fh = open(
+            f"{self.logger_path}/logs/{self.model_name}.log", "w"
+        )
+
+    @property
+    def model_name(self):
+        config_args = [getattr(self, source) for source in self.model_config_args]
+        return "_".join(config_args)
+
+    @property
+    def model_config_args(self):
+        return [
+            "name",
+            "batch_size",
+            "lr",
+            "gamma",
+            "seed",
+        ] + self.additional_model_config_args
+
+    @torch.enable_grad()
+    def train(self, epoch, optimizer, **kwargs):
+        """
+        Train step of model returned by model_builder
+        """
+        raise NotImplementedError(
+            "Class must be overridden and train should be implemented"
+        )
+
+    @torch.no_grad()
+    def test(self, epoch, optimizer, **kwargs):
+        """
+        Test step of model returned by model_builder
+        """
+        raise NotImplementedError(
+            "Class must be overridden and test should be implemented"
+        )
+
+    def main(self):
+        """
+        Method that runs the main training and testing loop
+        """
+
+        train_loader, valid_loader = self.train_loader, self.valid_loader
+        set_seeds(self.seed)
+        net = self.net
+
+        start_epoch = 0
+
+        if self.resume:
+            # Load checkpoint.
+            print("Resuming from checkpoint at save/best.pth.tar...")
+            assert os.path.isdir(
+                self.models_path
+            ), "Error: no checkpoint directory found!"
+            checkpoint = torch.load(
+                os.path.join(self.models_path, f"{self.model_name}.best.pt")
+            )
+            net.load_state_dict(checkpoint["net"])
+            best_loss = checkpoint["test_loss"]
+            self.global_step = start_epoch * len(train_loader.dataset)
+
+        optimizer = optim.Adam(net.parameters(), lr=self.lr)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=self.gamma)
+
+        count_valid_not_improving = 0
+
+        for epoch in range(start_epoch, start_epoch + self.num_epochs):
+
+            loss = self.train(epoch, optimizer)
+            vld_loss = self.test(epoch, optimizer)
+
+            scheduler.step()
+
+            if not np.isnan(vld_loss):
+                self.log_fh.write(f"{epoch},{loss},{vld_loss}\n")
+
+            # count for early stopping
+            if vld_loss >= best_loss:
+                count_valid_not_improving += 1
+            else:
+                count_valid_not_improving = 0
+
+            if count_valid_not_improving > self.early_stopping_lim:
+                logging.info(f"Early stopping implemented at epoch #: {epoch}")
+                break
+
+            if np.isnan(vld_loss):
+                logging.info(f"Early stopping: valid loss is NAN")
+                break
+
+            if self.num_test_samples > 0:
+                # TODO implement sampler here to generate test samples
+                pass
+
+        self.log_fh.close()
+
+        state_curr = {"net": net.state_dict()}
+        torch.save(
+            state_curr, os.path.join(self.models_path, f"{self.model_name}.final.pt")
+        )
+
+
+def set_seeds(seed):
+    """
+    Set the seeds for reproducibility
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    return True
+
+
+def init_device(use_cuda_flag):
+    """
+    Get the device from input args
+    """
+    use_cuda = use_cuda_flag and torch.cuda.is_available()
+    if use_cuda_flag and not use_cuda:
+        raise_cuda_error()
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+    return device
+
+
+def create_folder_if_not_exists(path):
+    """
+    Helper to safely create folders along the path
+    """
+    if not os.path.exists(path):
+        logging.info(f"{path} does not exist, creating...")
+        os.makedirs(path, exist_ok=True)
+    return path
