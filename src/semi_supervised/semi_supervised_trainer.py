fix log prob calculation and add an empty semantic loss method

src/semi_supervised/semi_supervised_trainer.py
@@ -188,7 +188,7 @@ class SemiSupervisedTrainer(GenerativeTrainer):
                 net_args = net((data, one_hot))
                 loss = self.labeled_loss(data, one_hot, **net_args)
 
-                if not saved:
+                if not saved and not self.tqdm_print: #only save these if on local
                     save_image(torch.sigmoid(net_args["reconstructed"][0]), f'{self.figure_path}/recon_{epoch}.png')
                     save_image(data, f'{self.figure_path}/true_{epoch}.png')
                     saved = True