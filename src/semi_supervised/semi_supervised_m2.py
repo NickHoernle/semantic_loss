5a1f9b224ad004d3f1a41309994a2c1fa0f18dbc
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-05-22 14:23:22 +0100

    anneal even more]

--- src/semi_supervised/semi_supervised_m2.py
+++ src/semi_supervised/semi_supervised_m2.py
@@ -158,7 +158,7 @@ class M2SemiSupervisedTrainer(SemiSupervisedTrainer):
             loss_s_u += torch.where(distances < 20, 20 - distances, torch.zeros_like(distances))
 
         if epoch < 10:
-            return epoch/100 * (loss_s_l.sum() + loss_s_u.sum())
+            return epoch/1000 * (loss_s_l.sum() + loss_s_u.sum())
         return (loss_s_l.sum() + loss_s_u.sum())
 
 
