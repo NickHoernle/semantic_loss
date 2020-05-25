a5b3b23cb51b33d42ee49213453535b142e35593
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-05-25 08:49:15 +0100

    add a normalization to the cifar10 dataset

--- src/semi_supervised/semi_supervised_trainer.py
+++ src/semi_supervised/semi_supervised_trainer.py
@@ -249,10 +249,12 @@ class SemiSupervisedTrainer(GenerativeTrainer):
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
 
         _CIFAR_TEST_TRANSFORMS = [
             transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ]
 
         if self.dataset == "MNIST":
