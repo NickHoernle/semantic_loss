309ff5edd809edc62f0aa96344e235db0e3c42af
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-05-26 16:47:31 +0100

    increase the penalty on sloss

--- src/semi_supervised/semi_supervised_vae.py
+++ src/semi_supervised/semi_supervised_vae.py
@@ -193,7 +193,7 @@ class VAESemiSupervisedTrainer(SemiSupervisedTrainer):
             distances = torch.sqrt(torch.square(net.q_global_means[j] - net.q_global_means[idxs[idxs != j]]).sum(dim=1))
             sloss += torch.where(distances < 20, 20 - distances, torch.zeros_like(distances)).sum()
 
-        return sloss
+        return 1e2*sloss
 
     @staticmethod
     def simple_loss(data, reconstructed, latent_samples, q_vals):
