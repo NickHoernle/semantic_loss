fde22d26726d83a13aba666ef486cd01bf34bb54
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-05-27 11:05:55 +0100

    change strengths of init learning rates

--- scripts/generate_experiments.py
+++ scripts/generate_experiments.py
@@ -21,7 +21,7 @@ base_call = (f"semi_supervised_vae.py gmm --input-data={DATA_HOME}/data --output
 
 repeats = 1
 learning_rates = [1e-3, 1e-4]
-learning_rates2 = [1e-4, 1e-6, 1e-8]
+learning_rates2 = [1e-2, 1e-3, 1e-4]
 gammas = [.99, .999]
 hidden_dim = [500, 1500, 2500]
 kernel_nums= [100, 200, 300]
