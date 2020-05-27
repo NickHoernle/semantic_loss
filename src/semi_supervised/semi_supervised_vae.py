f39017ec42a4df470959242a96f82865e2ec436e
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-05-27 11:04:28 +0100

    evolve the SLOSS strength

--- src/semi_supervised/semi_supervised_vae.py
+++ src/semi_supervised/semi_supervised_vae.py
@@ -195,7 +195,7 @@ class VAESemiSupervisedTrainer(SemiSupervisedTrainer):
             distances = torch.sqrt(torch.square(net.q_global_means[j] - net.q_global_means[idxs[idxs != j]]).sum(dim=1))
             sloss += torch.where(distances < 20, 20 - distances, torch.zeros_like(distances)).sum()
 
-        return 1e3*sloss
+        return epoch*epoch*sloss
 
     @staticmethod
     def simple_loss(data, reconstructed, latent_samples, q_vals):
