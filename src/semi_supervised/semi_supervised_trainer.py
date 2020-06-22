3a5e6c7fddaedd41cdaf80f05ca09cf491e3e24d
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-06-22 15:38:26 +0100

    bugfix

--- src/semi_supervised/semi_supervised_trainer.py
+++ src/semi_supervised/semi_supervised_trainer.py
@@ -188,10 +188,11 @@ class SemiSupervisedTrainer(GenerativeTrainer):
                 progress_bar.set_postfix(nll=loss_meter.avg, sloss=sloss_meter.avg)
                 progress_bar.update(data_u.size(0))
 
+                self.global_step += data_u.size(0)
+
         if net.reg > 1:
             net.reg -= 1
-
-                self.global_step += data_u.size(0)
+            
         print(f"===============> Epoch {epoch}; SLoss: {sloss_meter.avg}; NLL: {loss_meter.avg/(self.batch_size*32*32)}")
         return loss_meter.avg
 
