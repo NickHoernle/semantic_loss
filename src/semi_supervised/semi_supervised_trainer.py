628d1cfe6987f2245d00ee06b3ad699355894004
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-06-23 07:37:33 +0100

    warmup for longer

--- src/semi_supervised/semi_supervised_trainer.py
+++ src/semi_supervised/semi_supervised_trainer.py
@@ -101,10 +101,10 @@ class SemiSupervisedTrainer(GenerativeTrainer):
         transform = transforms.Compose([RandomCrop(32, padding=4, device=self.device),
                                         RandomHorizontalFlip(device=self.device)])
 
-        if epoch < 5:
+        if epoch < 10:
             for param_group in opt_unsup.param_groups:
-                param_group['lr'] = 1e-8
-        elif epoch == 5:
+                param_group['lr'] = 5e-8
+        elif epoch == 10:
             for param_group in opt_unsup.param_groups:
                 param_group['lr'] = self.lr
         # anneal the tau parameter
