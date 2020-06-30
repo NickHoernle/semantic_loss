6a682f99e016879090782375fed6ae5927cb7c0a
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-06-30 14:27:54 +0100

    that was a bad idea

--- src/vaelib/vae.py
+++ src/vaelib/vae.py
@@ -14,14 +14,9 @@ from torch.distributions import MultivariateNormal, Uniform, \
 import math
 
 def init_weights(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('Linear') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+    if (type(m) == nn.Linear) or (type(m) == nn.Conv2d) or (type(m) == nn.ConvTranspose2d):
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0)
 
 
 class Flatten(nn.Module):
