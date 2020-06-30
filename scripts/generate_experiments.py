715ede472798819d37fddc3f15ef4b6863f2c00d
Nick Hoernle <nicholas.hoernle@gmail.com>
2020-06-30 09:20:54 +0100

    fiddlign with params

--- scripts/generate_experiments.py
+++ scripts/generate_experiments.py
@@ -20,10 +20,10 @@ base_call = (f"semi_supervised_vae.py gmm --input-data={DATA_HOME}/data --output
              f"--dataset=CIFAR10 ")
 
 repeats = 1
-learning_rates = [5e-6, 1e-6]
-learning_rates2 = [1e-7]
+learning_rates = [1e-5, 5e-6]
+learning_rates2 = [1e-6, 5e-7]
 gammas = [.99]
-hidden_dim = [100]
+hidden_dim = [500]
 kernel_nums= [250, 500, 750]
 sloss = [True]
 batch_size = [100]
