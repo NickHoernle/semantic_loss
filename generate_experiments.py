8252375fce7fcb98aa21abe81aad8ed0c222cbf3
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-02-12 16:30:09 +0000

    make a wild experiment

--- generate_experiments.py
+++ generate_experiments.py
@@ -21,7 +21,7 @@ repeats = 1
 
 learning_rate = [.1, .075, .05, 0.25]
 sloss = [True]
-lower_lim = [-5, -10, -15, -20]
+lower_lim = [-5, -10, -15, -20, -50]
 
 settings = [(lr, sloss_, l_lim, rep)
             for lr in learning_rate
