b9aa5a71ceb74470413190f081fedf1716e96b4f
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-02-17 18:02:04 +0000

    change experiment params

--- generate_experiments.py
+++ generate_experiments.py
@@ -21,7 +21,7 @@ repeats = 1
 
 learning_rate = [0.25, .1, .075, .01]
 sloss = [True]
-lower_lim = [-5, -10, -20]
+lower_lim = [-4, -5, -7]
 upper_lim = [-1, -2, -3]
 
 settings = [(lr, sloss_, l_lim, u_lim, rep)
