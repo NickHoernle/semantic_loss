b33367512fce76c8984cce41a6180fd64df69e66
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-02-17 12:39:51 +0000

    change architecture slightly

--- symbolic.py
+++ symbolic.py
@@ -151,17 +151,19 @@ class OrList(nn.Module):
     def __init__(self, terms):
         super(OrList, self).__init__()
         self.layers = nn.ModuleList(terms)
-        self.fc = nn.Sequential(nn.Tanh(), nn.Linear(len(terms), len(terms)))
+        self.fc = nn.Linear(len(terms), len(terms))
 
     def threshold1p(self):
         for layer in self.layers:
             layer.threshold1p()
 
     def forward(self, x, class_prediction, test=False):
-        log_py = self.fc(class_prediction).log_softmax(dim=1)
         pred = torch.stack([f(x) for f in self.layers], dim=1)
+        log_py = self.fc(torch.cat((class_prediction, pred.max(dim=-1)[0]), dim=1)).log_softmax(dim=1)
+
         if test:
             return pred[np.arange(len(log_py)), log_py.argmax(dim=1)]
+
         return pred, log_py
 
 
