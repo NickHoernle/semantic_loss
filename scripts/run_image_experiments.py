9c9702396106a1cffa748dac5ce89aae191801cd
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-03 08:38:58 +0000

    reduce size of experiments file by breaking into images and synthetic

--- scripts/run_image_experiments.py
+++ scripts/run_image_experiments.py
@@ -1,6 +1,6 @@
 import fire
 
-from experiment.experiments import image_experiment_options
+from experiment.image_experiments import image_experiment_options
 
 if __name__ == '__main__':
     fire.Fire(image_experiment_options)
--- scripts/run_synthetic_data_experiments.py
+++ scripts/run_synthetic_data_experiments.py
@@ -1,6 +1,6 @@
 import fire
 
-from experiment.experiments import synthetic_experiment_options
+from experiment.synthetic_experiments import synthetic_experiment_options
 
 if __name__ == '__main__':
     fire.Fire(synthetic_experiment_options)
