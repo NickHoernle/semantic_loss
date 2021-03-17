31e511993393c9f0a08d47b3eeaa462292a8631d
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-17 12:39:45 +0000

    start logic lower

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -248,8 +248,8 @@ class ConstrainedMNIST(BaseMNISTExperiment):
                         ixs_not=[],
                         ixs_less_than=lwr_c,
                         threshold_upper=0.0,
-                        threshold_lower=-5.0,
-                        threshold_limit=-5.0,
+                        threshold_lower=-1.0,
+                        threshold_limit=-1.0,
                     )
                 )
         return terms
