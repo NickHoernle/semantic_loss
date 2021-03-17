71b60dcd9ad03ce6f56cd1351a9bc562c5a328a1
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-17 16:29:19 +0000

    smaller batch size

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -248,8 +248,8 @@ class ConstrainedMNIST(BaseMNISTExperiment):
                         ixs_not=[],
                         ixs_less_than=lwr_c,
                         threshold_upper=0.0,
-                        threshold_lower=-1.0,
-                        threshold_limit=-15.0,
+                        threshold_lower=-10.0,
+                        threshold_limit=-10.0,
                     )
                 )
         return terms
