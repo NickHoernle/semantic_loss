ff90b39c9d5cf4e329127fb1ab4bf7db576cdea6
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-18 11:33:04 +0000

    all thresholds to -1, -1, -1

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -306,9 +306,9 @@ class ConstrainedMNIST(BaseMNISTExperiment):
                         ixs1=constrain,
                         ixs_not=[],
                         ixs_less_than=lwr_c,
-                        threshold_upper=0.0,
-                        threshold_lower=-10.0,
-                        threshold_limit=-10.0,
+                        threshold_upper=-1,
+                        threshold_lower=-1,
+                        threshold_limit=-1,
                     )
                 )
         return terms
