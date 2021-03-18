1f90e4862fa6e074850d31b489566dca275f2390
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-18 10:12:42 +0000

    lower to -10, -5

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -307,7 +307,7 @@ class ConstrainedMNIST(BaseMNISTExperiment):
                         ixs_not=[],
                         ixs_less_than=lwr_c,
                         threshold_upper=0.0,
-                        threshold_lower=-5.0,
+                        threshold_lower=-10.0,
                         threshold_limit=-15.0,
                     )
                 )
