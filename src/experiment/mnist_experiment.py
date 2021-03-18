ef3d11deda7b02ba6b33fe8f5db55963bf8a1777
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-18 12:28:34 +0000

    leaking relu

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -306,9 +306,9 @@ class ConstrainedMNIST(BaseMNISTExperiment):
                         ixs1=constrain,
                         ixs_not=[],
                         ixs_less_than=lwr_c,
-                        threshold_upper=-1,
-                        threshold_lower=-1,
-                        threshold_limit=-15,
+                        threshold_upper=0,
+                        threshold_lower=-10,
+                        threshold_limit=-10,
                     )
                 )
         return terms
