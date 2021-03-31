16f6c2ec0efbb7d5dc4d236ccce521404dbbff44
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-03-31 14:51:33 +0100

    don't need the heuristic stuff?

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -361,8 +361,8 @@ class ConstrainedMNIST(BaseMNISTExperiment):
         recon_losses, labels = llik.min(dim=1)
 
         loss = (logpy.exp() * (llik + logpy)).sum(dim=-1).mean()
-        loss += recon_losses.mean()
-        loss += F.nll_loss(logpy, labels)
+        # loss += recon_losses.mean()
+        # loss += F.nll_loss(logpy, labels)
 
         # llik = ((lp1.exp() * (ll1 + lp1)).sum(dim=-1) +
         #         (lp2.exp() * (ll2 + lp2)).sum(dim=-1) +
