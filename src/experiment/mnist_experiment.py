2942d55ec12f636974db782d9476001107998fad
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-04-01 09:38:27 +0100

    does this work with the latest addition

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -349,18 +349,18 @@ class ConstrainedMNIST(BaseMNISTExperiment):
         for k, vals in knowledge.items():
             for v0, v1 in vals:
                 
-                # weight1 = (torch.ones_like(lp1[:, v0]) + lp1[:, v0].exp()).detach() - lp1[:, v0].exp()
-                # weight2 = (torch.ones_like(lp2[:, v1]) + lp2[:, v1].exp()).detach() - lp2[:, v1].exp()
-                # weight3 = (torch.ones_like(lp3[:, k])  + lp3[:, k].exp()).detach()  - lp3[:, k].exp()
+                weight1 = (torch.ones_like(lp1[:, v0]) + lp1[:, v0].exp()).detach() - lp1[:, v0].exp()
+                weight2 = (torch.ones_like(lp2[:, v1]) + lp2[:, v1].exp()).detach() - lp2[:, v1].exp()
+                weight3 = (torch.ones_like(lp3[:, k])  + lp3[:, k].exp()).detach()  - lp3[:, k].exp()
                 
                 llik += [
                     (
-                        # weight3*ll3[:, k] + weight1*ll1[:, v0] + weight2*ll2[:, v1]
-                        ll3[:, k] + ll1[:, v0] + ll2[:, v1]
+                        weight3*ll3[:, k] + weight1*ll1[:, v0] + weight2*ll2[:, v1]
+                        # ll3[:, k] + ll1[:, v0] + ll2[:, v1]
                         # (ll3[:, k] + weight3 * ll3[:, k]).detach() + weight3 * ll3[:, k] +
                         # (ll1[:, v0] + weight1 * ll1[:, v0]).detach() + weight1 * ll1[:, v0] +
                         # (ll2[:, v1] + weight2 * ll2[:, v1]).detach() + weight2 * ll2[:, v1]
-                        - lp3[:, k] - lp1[:, v0] - lp2[:, v1]
+                        # - lp3[:, k] - lp1[:, v0] - lp2[:, v1]
                     ) / 3
                 ]
 
