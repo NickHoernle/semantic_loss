88c23e276f4c3666cec84f3b5e14fbe60f0c61bf
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-04-01 08:34:58 +0100

    remove the stright through estimator

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -348,13 +348,15 @@ class ConstrainedMNIST(BaseMNISTExperiment):
 
         for k, vals in knowledge.items():
             for v0, v1 in vals:
-                weight1 = lp1[:, v0].exp().detach()
-                weight2 = lp2[:, v1].exp().detach()
-                weight3 = lp3[:, k].exp().detach()
+                # weight1 = lp1[:, v0].exp().detach()
+                # weight2 = lp2[:, v1].exp().detach()
+                # weight3 = lp3[:, k].exp().detach()
                 llik += [
-                    ((ll3[:, k] - weight3 * ll3[:, k]).detach() + weight3 * ll3[:, k] +
-                     (ll1[:, v0] - weight1 * ll1[:, v0]).detach() + weight1 * ll1[:, v0] +
-                     (ll2[:, v1] - weight2 * ll2[:, v1]).detach() + weight2 * ll2[:, v1]
+                    (
+                        ll3[:, k] + ll1[:, v0] + ll2[:, v1]
+                        # (ll3[:, k] + weight3 * ll3[:, k]).detach() + weight3 * ll3[:, k] +
+                        # (ll1[:, v0] + weight1 * ll1[:, v0]).detach() + weight1 * ll1[:, v0] +
+                        # (ll2[:, v1] + weight2 * ll2[:, v1]).detach() + weight2 * ll2[:, v1]
                     - lp3[:, k] - lp1[:, v0] - lp2[:, v1]) / 3
                 ]
 
