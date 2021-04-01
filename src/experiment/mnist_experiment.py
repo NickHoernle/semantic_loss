3b4ce443459216e68175167d999a56cda5e2d4d8
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-04-01 10:20:59 +0100

    signs the other way

--- src/experiment/mnist_experiment.py
+++ src/experiment/mnist_experiment.py
@@ -349,15 +349,15 @@ class ConstrainedMNIST(BaseMNISTExperiment):
         for k, vals in knowledge.items():
             for v0, v1 in vals:
                 
-                weight1 = (torch.zeros_like(lp1[:, v0]) + lp1[:, v0]).detach() - lp1[:, v0]
-                weight2 = (torch.zeros_like(lp2[:, v1]) + lp2[:, v1]).detach() - lp2[:, v1]
-                weight3 = (torch.zeros_like(lp3[:, k])  + lp3[:, k]).detach()  - lp3[:, k]
+                weight1 = (torch.zeros_like(lp1[:, v0]) - lp1[:, v0]).detach() + lp1[:, v0]
+                weight2 = (torch.zeros_like(lp2[:, v1]) - lp2[:, v1]).detach() + lp2[:, v1]
+                weight3 = (torch.zeros_like(lp3[:, k])  - lp3[:, k]).detach()  + lp3[:, k]
                 
                 llik += [
                     (   
-                        weight1.exp()*(ll1[:, v0] + weight1)+ 
-                        weight2.exp()*(ll2[:, v1] + weight2)+
-                        weight3.exp()*(ll3[:, k] + weight3) 
+                        weight1.exp()*(ll1[:, v0] - weight1)+ 
+                        weight2.exp()*(ll2[:, v1] - weight2)+
+                        weight3.exp()*(ll3[:, k] - weight3) 
                         # ll3[:, k] + ll1[:, v0] + ll2[:, v1]
                         # (ll3[:, k] + weight3 * ll3[:, k]).detach() + weight3 * ll3[:, k] +
                         # (ll1[:, v0] + weight1 * ll1[:, v0]).detach() + weight1 * ll1[:, v0] +
