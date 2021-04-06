9838fdef3dd1e3fd0a9868f3a3239f7445c99317
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-04-06 13:54:14 +0100

    use softmax in final layer

--- src/experiment/generative.py
+++ src/experiment/generative.py
@@ -240,7 +240,9 @@ class ConstrainedMnistVAE(MnistVAE):
         d2 = self.decode(encoded2)
         d3 = self.decode(encoded3)
 
-        cp = torch.cat((log_pred1, log_pred2, log_pred3), dim=1)
+        cp = torch.cat((log_pred1.softmax(dim=1),
+                        log_pred2.softmax(dim=1),
+                        log_pred3.softmax(dim=1)), dim=1)
 
         logic_pred, lpy = self.logic_decoder(cp, self.logic_pred(cp))
         log_p1, log_p2, log_p3 = log_pred1, log_pred2, log_pred3
