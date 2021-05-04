041fe3f8e6bfbabc3ba2bfa61979d580c796c78d
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-05-04 16:54:13 +0100

    use relu

--- src/experiment/generative.py
+++ src/experiment/generative.py
@@ -229,7 +229,7 @@ class ConstrainedMnistVAE(MnistVAE):
         self.logic_decoder = OrList(terms=terms)
 
         self.logic_pred = nn.Sequential(
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.BatchNorm1d(3 * self.num_labels),
             nn.Linear(3 * self.num_labels, len(terms)),
         )
