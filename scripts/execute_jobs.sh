7e79c72de23b5e0084568948f692b5d39397d80c
Nick Hoernle <nicholas.hoernle@gmail.com>
2021-05-14 11:46:31 +0100

    need longer for these experiments

--- scripts/execute_jobs.sh
+++ scripts/execute_jobs.sh
@@ -3,5 +3,5 @@
 EXPT_FILE=experiment.txt
 NR_EXPTS=`cat ${EXPT_FILE} | wc -l`
 MAX_PARALLEL_JOBS=20
-sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} --time=7:30:00 dispatch_job.sh $EXPT_FILE
-#sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} --time=7:30:00 --partition=PGR-Standard dispatch_job.sh $EXPT_FILE
+#sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} --time=7:30:00 dispatch_job.sh $EXPT_FILE
+sbatch --array=1-${NR_EXPTS}%${MAX_PARALLEL_JOBS} --time=1-00:00:00 --partition=PGR-Standard dispatch_job.sh $EXPT_FILE
